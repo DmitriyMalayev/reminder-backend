2b10a5bc7465eb8da1d92758de8cc6d4e29c5076
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-01-06 18:39:48 -0500

    routes, devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :path: "", path_names: {
+    sign_in: "login",
+    sign_out: "logout", 
+    registration: "signup"
+  }, 
+  controllers: {
+    sessions: "users/sessions", 
+    registrations: "users/registrations"
+  }
 end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
